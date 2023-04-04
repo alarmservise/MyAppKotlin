1ac7ca28bcecde0de5fb82984bee4d0f840fc26e
alarmservise <alarmservise@mail.ru>
2023-04-04 21:35:56 +0300

    глава 2 стр 85 Упражнение. Добавление кнопки возврата

--- app/src/main/java/com/alarmservise/myappkotlin/MainActivity.kt
+++ app/src/main/java/com/alarmservise/myappkotlin/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
+    private lateinit var prevButton: Button
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -32,30 +33,28 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.prev_button)
+
         questionTextView = findViewById(R.id.question_text_view)
 
 
 
         trueButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
-                R.string.correct_toast,
-                Toast.LENGTH_SHORT
-            )
-                .show()
+            checkAnswer(true)
         }
 
         falseButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
-                R.string.incorrect_toast,
-                Toast.LENGTH_SHORT
-            )
-                .show()
+            checkAnswer(false)
         }
 
         nextButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+        prevButton.setOnClickListener {
+            if (currentIndex > 1) {
+            currentIndex = (currentIndex - 1) % questionBank.size}
+            else {currentIndex = 0}
             updateQuestion()
         }
 
@@ -65,5 +64,15 @@ class MainActivity : AppCompatActivity() {
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+        val messsageResIdb= if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messsageResIdb, Toast.LENGTH_SHORT).show()
     }
 }
